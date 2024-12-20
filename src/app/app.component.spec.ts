328ddfffd293a4964e3ea93dbbb9f1f5989eaebf
Josiassantos1577 <josiassantos1577@gmail.com>
2024-12-19 23:34:32 -0300

    feat: Criando app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'cards-estilizados-com-diretivas' title`, () => {
+  it(`should have as title 'card-com-projecao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cards-estilizados-com-diretivas');
+    expect(app.title).toEqual('card-com-projecao');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cards-estilizados-com-diretivas');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('card-com-projecao app is running!');
   });
 });
